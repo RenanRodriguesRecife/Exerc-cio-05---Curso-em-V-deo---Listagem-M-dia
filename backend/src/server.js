18d94f04b778f5434e65e54dc56f58abdb676335
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-03-01 15:35:51 -0300

    methodos e tipos de parametros das requisições http

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,26 @@
 const express = require('express')
 const app = express();
 
+// req.query = Acessar query params (?idade=3 (filtros))
+// req.params = Acessar route param (para edição e delete)
+// req.body = Acessar corpo da requisição (para criação, edição)
+
+app.use(express.json())
+
 app.get('/',(req,res)=>{
     return res.json({message: "Hello World"});
 })
 
 app.post('/users',(req,res)=>{
-    return res.json({message: "Hello omnistack"});
+    return res.json({req.body});
+})
+
+app.get('/users',(req,res)=>{
+    return res.json({id: req.query.idade});
+})
+
+app.put('/users/:id',(req,res)=>{
+    return res.json({id: req.params.id});
 })
 
 app.listen(3333);
